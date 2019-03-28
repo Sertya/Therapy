34eba655f875fdf250a88e19c76db863e452b73a
Iryna Dauchyonas <irynadauchyonas@MacBook-Air-Iryna.local>
2019-03-28 15:33:14 +0200

    Added index.html script.js style.css

---
+++ script.js
@@ -0,0 +1,4 @@
+menu.onclick = function menuClassName() {
+    let x = document.getElementById('navigation');
+    x.className === 'topnav' ? x.className = 'topnav responsive' : x.className = 'topnav';
+}
